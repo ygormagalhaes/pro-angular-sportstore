96307f854ad2c2ab7ab64cd1a9ba83f75313815b
Ygor <emaildoygor@gmail.com>
2019-07-01 19:01:48 -0300

    Criação dos componentes de edição de produtos e pedidos na área de admin com children property pararotas com router-outlet aninhados

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../model/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: 'admin.component.html'
 })
 export class AdminComponent {
 
+    constructor(private auth: AuthService, private router: Router) { }
+
+    logout() {
+        this.auth.clear();
+        this.router.navigateByUrl('/');
+    }
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,3 +1,5 @@
+import { ProductEditorComponent } from './product-editor.component';
+import { ProductTableComponent } from './product-table.component';
 import { AuthGuard } from './auth.guard';
 import { RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
@@ -5,16 +7,28 @@ import { FormsModule } from '@angular/forms';
 import { AuthComponent } from './auth.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrderTableComponent } from './order-table.component';
 
 const routing = RouterModule.forChild([
     { path: 'auth', component: AuthComponent },
-    { path: 'main', component: AdminComponent, canActivate: [AuthGuard] },
+    {
+        path: 'main',
+        component: AdminComponent,
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'products/:mode/:id', component: ProductEditorComponent },
+            { path: 'products/:mode', component: ProductEditorComponent },
+            { path: 'products', component: ProductTableComponent },
+            { path: 'orders', component: OrderTableComponent },
+            { path: '**', redirectTo: 'products' }
+        ]
+    },
     { path: '**', redirectTo: 'auth' }
 ]);
 
 @NgModule({
     imports: [FormsModule, CommonModule, routing],
-    declarations: [AuthComponent, AdminComponent],
+    declarations: [AuthComponent, AdminComponent, ProductTableComponent, OrderTableComponent, ProductEditorComponent],
     providers: [AuthGuard]
 })
 export class AdminModule { }
