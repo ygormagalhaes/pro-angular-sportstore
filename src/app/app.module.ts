8a9bc5818d48efd1118d5d5fe8726aff6e9ac857
Ygor <emaildoygor@gmail.com>
2019-07-01 11:57:48 -0300

    Import do módulo store no root module e atualização do template do root component para exibir os dados do store component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { StoreModule } from './store/store.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
