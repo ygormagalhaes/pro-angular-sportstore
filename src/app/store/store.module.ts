ea6c0bb573e4de878f7fc4ee631df6605514ddb9
Ygor <emaildoygor@gmail.com>
2019-07-01 15:18:54 -0300

    Criação de componentes para detalhes do carrinho e checkout de pedido

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,3 +1,5 @@
+import { CheckoutComponent } from './checkout.component';
+import { CartDetailComponent } from './cart-detail.component';
 import { CartSummaryComponent } from './cart-summary.component';
 import { CounterDirective } from './counter.directive';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,7 +10,7 @@ import { StoreComponent } from './store.component';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
-    exports: [StoreComponent]
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent, CartDetailComponent, CheckoutComponent],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 export class StoreModule { }
