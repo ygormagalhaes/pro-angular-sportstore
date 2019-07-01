57f7c983b8d919f6cf4c7a50de6bd91a26961fc8
Ygor <emaildoygor@gmail.com>
2019-07-01 17:03:38 -0300

    Criação de repositório para obtenção e persistência de pedidos

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,8 +2,9 @@ import { Cart } from './cart.model';
 import { NgModule } from '@angular/core';
 import { StaticDataSource } from './static.datasource';
 import { ProductRepository } from './product.repository';
+import { OrderRepository } from './order.repository';
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart]
+    providers: [ProductRepository, StaticDataSource, Cart, OrderRepository]
 })
 export class ModelModule { }
