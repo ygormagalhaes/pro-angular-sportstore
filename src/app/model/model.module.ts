e144ca9d14297b89c160dc7509b2f2f016a11c59
Ygor <emaildoygor@gmail.com>
2019-07-01 14:45:33 -0300

    Criação de serviço para carrinho de compras

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,8 +1,9 @@
+import { Cart } from './cart.model';
 import { NgModule } from '@angular/core';
 import { StaticDataSource } from './static.datasource';
 import { ProductRepository } from './product.repository';
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource]
+    providers: [ProductRepository, StaticDataSource, Cart]
 })
 export class ModelModule { }
