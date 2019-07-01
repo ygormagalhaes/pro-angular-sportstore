39557ea7f767718e4d504b4a0e930ffe1caa7954
Ygor <emaildoygor@gmail.com>
2019-07-01 15:50:35 -0300

    Criação de guard para que roda de carrinho ou checkout não seja chamada se for primeira navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreComponent } from './store/store.component';
+import { StoreFirstGuard } from './store-first.guard';
 
 @NgModule({
   declarations: [
@@ -16,13 +17,13 @@ import { StoreComponent } from './store/store.component';
     BrowserModule,
     StoreModule,
     RouterModule.forRoot([
-      { path: 'store', component: StoreComponent },
-      { path: 'cart', component: CartDetailComponent },
-      { path: 'checkout', component: CheckoutComponent },
+      { path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard] },
+      { path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard] },
+      { path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard] },
       { path: '**', redirectTo: '/store' }
     ])
   ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
