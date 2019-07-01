3f88261362007e8a5c134b89df4187a0524796c1
Ygor <emaildoygor@gmail.com>
2019-07-01 11:17:26 -0300

    Ajustes iniciais do root componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app',
+  template: `<div class="bg-success p-2 text-center text-white">
+              This is SportsStore
+            </div>`
 })
 export class AppComponent {
   title = 'SportStore';
