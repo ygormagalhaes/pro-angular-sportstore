2b53b8f22258312e500540cb6b63fc0b2bf0e10e
Ygor <emaildoygor@gmail.com>
2019-07-01 17:21:07 -0300

    Implementações relativas ao checkout dos pedidos

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { StaticDataSource } from './static.datasource';
 import { ProductRepository } from './product.repository';
 import { OrderRepository } from './order.repository';
+import { Order } from './order.model';
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart, OrderRepository]
+    providers: [ProductRepository, StaticDataSource, Cart, OrderRepository, Order]
 })
 export class ModelModule { }
