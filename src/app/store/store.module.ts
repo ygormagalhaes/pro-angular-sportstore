ca6dd7c15db484668ae415ab2227a263120c0d3d
Ygor <emaildoygor@gmail.com>
2019-07-01 13:23:53 -0300

    Ajuste no template de store component para listagem de produtos e suas informações

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,10 +1,11 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ModelModule } from './../model/model.module';
 import { FormsModule } from '@angular/forms';
 import { StoreComponent } from './store.component';
 
 @NgModule({
-    imports: [ModelModule, FormsModule],
+    imports: [ModelModule, BrowserModule, FormsModule],
     declarations: [StoreComponent],
     exports: [StoreComponent]
 })
