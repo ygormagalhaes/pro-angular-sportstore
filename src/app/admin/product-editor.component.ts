4719881cdcecb858db206969f3d748da117dc37d
Ygor <emaildoygor@gmail.com>
2019-07-09 17:27:05 -0300

    Implementações para edição de produtos na área administrativa

--- src/app/admin/product-editor.component.ts
+++ src/app/admin/product-editor.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
-    template: `<div class="bg-warning p-2 text-white">
-                <h3>Product Editor Placeholder</h3>
-              </div>`
+  templateUrl: 'product-editor.component.html'
 })
-export class ProductEditorComponent { }
+export class ProductEditorComponent {
+  editing: boolean = false;
+  product: Product = new Product();
+  constructor(private repository: ProductRepository,
+    private router: Router,
+    activeRoute: ActivatedRoute) {
+    this.editing = activeRoute.snapshot.params['mode'] === 'edit';
+    if (this.editing) {
+      Object.assign(this.product,
+        repository.getProduct(Number.parseInt(activeRoute.snapshot.params['id'], 10)));
+    }
+  }
+
+  save(form: NgForm) {
+    this.repository.saveProduct(this.product);
+    this.router.navigateByUrl('/admin/main/products');
+  }
+}
